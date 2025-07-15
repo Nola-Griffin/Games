80f4d5f63b103e9d65f96062269d1251ffaac1e1
Nola-Griffin <xiangyun35@icloud.com>
2025-07-14 20:41:27 -0400

    Add files via upload
    The Main class now has if-else-if statements nested in a while loop to accept user Inputs and return the appropriate message depending on the game of feature that you selected. Since none of the games are available (will add first game tomorrow), all of the options have error messages notifying the user that the game has not been added yet. The quit option allows the user to quit the program. There is an else statement that covers alternate (unintended) user errors at a basic level. The user input is adjusted so that the program will not care about uppercase and lowercase letters. I am already thinking about how I am going to tackle the potential problem of users wanting to play multiple games in one go. I changed the Outputs.java file class name to OutputsInputs.java because I am allowing this class to accept certain user inputs as well. I will figure it out, maybe do another boolean variable with a while loop.

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@
  * @author Nola Griffin
  * @version 07/13/2025
  */
+import java.util.*;
 public class Main
 {
+    //initializing class scanner variable
+    static Scanner in = new Scanner (System.in);
+    
     /**
      * Main Method
      */
@@ -16,16 +20,59 @@ public class Main
         String userInput;
         
         //calling of methods
-        Outputs.printIntro(); 
-        userInput = Outputs.printMenu();
+        OutputsInputs.printIntro(); 
+        userInput = OutputsInputs.printMenu();
+        
     }
     
     //additional methods that help with method calls
     /**
      * Method that takes what the user input and calls the methods to start the corrosponding game.
      */
-    public static void playGames()
+    public static void playGames(String userInput)
     {
+        //initializing local boolean variable
+        boolean isAcceptable = false;
         
+        //while loop to determine if the input is acceptable
+        while(isAcceptable == false)
+        {
+            //eliminate capital lowercase differences
+            userInput = userInput.toUpperCase();
+        
+            //if statements to determine which game to play
+            if(userInput.equals("A"))
+            {
+                isAcceptable = true;
+                System.out.println("Thank you for using the gaming program! Have a nice day!");
+            }
+            else if(userInput.equals("B"))
+            {
+                isAcceptable = true;
+                System.out.println("I have not added this feature, I am glad that you are excited! It will be added soon ;)");
+            }
+            else if(userInput.equals("C"))
+            {
+                isAcceptable = true;
+                System.out.println("I have not added this feature, I am glad that you are excited! It will be added soon ;)");
+
+            }
+            else if(userInput.equals("D"))
+            {
+                isAcceptable = true;
+                System.out.println("I have not added this feature, I am glad that you are excited! It will be added soon ;)");
+
+            }
+            else if(userInput.equals("E"))
+            {
+                isAcceptable = true;
+                System.out.println("I have not added this feature, I am glad that you are excited! It will be added soon ;)");
+            }
+            else
+            {
+                System.out.println("There seems to have been a mistake when inputting, please try again.");
+                userInput = OutputsInputs.printMenu();
+            }
+        }
     }
 }
